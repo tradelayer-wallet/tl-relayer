c2b35d33ba1a5707d1f363aee3ce7e584160124e
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2024-12-22 16:21:47 -0300

    Update token.route.ts

--- src/routes/token.route.ts
+++ src/routes/token.route.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyRequest } from "fastify";
 import { getTokenInfo, listTokens } from "../services/token.service";
 
 export const tokenRoute = (fastify: FastifyInstance, opts: any, done: any) => {
@@ -7,20 +7,22 @@ export const tokenRoute = (fastify: FastifyInstance, opts: any, done: any) => {
             const res = await listTokens();
             reply.send(res);
         } catch (error: unknown) {
-            reply.send({ error: error.message });
+            const errorMessage = error instanceof Error ? error.message : 'An unexpected error occurred';
+            reply.status(500).send({ error: errorMessage });
         }
     });
 
-    fastify.get('/:propid', async (request, reply) => {
+    fastify.get('/:propid', async (request: FastifyRequest<{ Params: { propid: string } }>, reply) => {
         try {
-            const { propid } = request.params as { propid: string };
+            const { propid } = request.params;
             const _propid = parseInt(propid);
             const res = await getTokenInfo(_propid);
             reply.send(res);
         } catch (error: unknown) {
-            reply.send({ error: error.message });
+            const errorMessage = error instanceof Error ? error.message : 'An unexpected error occurred';
+            reply.status(500).send({ error: errorMessage });
         }
     });
 
     done();
-}
+};
