ff46dd6cc19829181dc0e5cf834dc7395946ed61
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2024-12-22 15:55:20 -0300

    Update env.config.ts

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -14,11 +14,11 @@ export const envConfig: {
     RPC_HOST: string;
     RPC_PORT: number;
 } = {
-    SERVER_PORT: parseInt(process.env.SERVER_PORT),
-    SOCKET_PORT: parseInt(process.env.SOCKET_PORT),
-    NETWORK: process.env.NETWORK,
-    RPC_USER: process.env.RPC_USER,
-    RPC_PASS: process.env.RPC_PASS,
-    RPC_HOST: process.env.RPC_HOST,
-    RPC_PORT: parseInt(process.env.RPC_PORT),
+    SERVER_PORT: parseInt(process.env.SERVER_PORT || '8000', 10), // Default to 3000 if not set
+    SOCKET_PORT: parseInt(process.env.SOCKET_PORT || '8001', 10), // Default to 3001 if not set
+    NETWORK: process.env.NETWORK || 'TESTNET', // Default to 'TESTNET' if not set
+    RPC_USER: process.env.RPC_USER || 'defaultUser', // Provide a default placeholder
+    RPC_PASS: process.env.RPC_PASS || 'defaultPass', // Provide a default placeholder
+    RPC_HOST: process.env.RPC_HOST || 'localhost', // Default to 'localhost' if not set
+    RPC_PORT: parseInt(process.env.RPC_PORT || '8332', 10), // Default to a common RPC port (e.g., Bitcoin's default)
 };
