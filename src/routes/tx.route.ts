46a4efbbf06256798feb13c278cef393e8a5b5c9
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2024-12-22 16:22:28 -0300

    Update tx.route.ts

--- src/routes/tx.route.ts
+++ src/routes/tx.route.ts
@@ -1,16 +1,17 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyRequest } from "fastify";
 import { getTx } from "../services/tx.service";
 
 export const txRoute = (fastify: FastifyInstance, opts: any, done: any) => {
-    fastify.get('/:txid', async (request, reply) => {
+    fastify.get('/:txid', async (request: FastifyRequest<{ Params: { txid: string } }>, reply) => {
         try {
-            const { txid } = request.params as { txid: string };
+            const { txid } = request.params;
             const res = await getTx(txid);
             reply.send(res);
         } catch (error: unknown) {
-            reply.send({ error: error.message });
+            const errorMessage = error instanceof Error ? error.message : 'An unexpected error occurred';
+            reply.status(500).send({ error: errorMessage });
         }
     });
 
     done();
-}
+};
