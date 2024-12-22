79dfabc4102891f0487ff23f227aae40b654dca5
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2024-12-22 09:39:27 -0300

    Update chain.service.ts

--- src/services/chain.service.ts
+++ src/services/chain.service.ts
@@ -1,6 +1,6 @@
 import { rpcClient } from "../config/rpc.config"
 
 export const getChainInfo = async () => {
-    const res = await rpcClient.call('tl_getinfo');
+    const res = await rpcClient.call('getblockchaininfo');
     return res;
 }
