0e779b05c92909eb4f90efb5542ff22441aa0b0b
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2024-12-30 22:02:30 -0300

    Update chain.route.ts

--- src/routes/chain.route.ts
+++ src/routes/chain.route.ts
@@ -13,31 +13,3 @@ export const chainRoute = (fastify: FastifyInstance, opts: any, done: any) => {
         }
     });
 
- 
-fastify.post(
-    "/check-ip",
-    async (
-      request: FastifyRequest<{ Body: { ip: string } }>, // Define the type of request body
-      reply
-    ) => {
-      try {
-        const body = request.body as { ip: string }; // Explicitly type-cast request.body
-        const { ip } = body;
-
-        if (!ip) {
-          reply.status(400).send({ error: "IP address is required" });
-          return;
-        }
-
-        const res = await checkIP(ip);
-        reply.send(res);
-      } catch (error: unknown) {
-        const errorMessage = error instanceof Error ? error.message : "An unexpected error occurred";
-        reply.status(500).send({ error: errorMessage });
-      }
-    }
-  );
-
-   done();
-};
-
