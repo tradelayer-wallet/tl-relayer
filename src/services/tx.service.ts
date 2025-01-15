99a9038d7a7b43a0c86c1ee1d7b11dc7495bb51f
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-01-15 15:55:23 -0300

    Update tx.service.ts

--- src/services/tx.service.ts
+++ src/services/tx.service.ts
@@ -394,23 +394,6 @@ export const buildLTCTradeTx = async (txConfig: IBuildLTCITTxConfig, isApiMode: 
       [buyerAddress]: safeNumber(totalInputAmount - amount - fee), // Change output for buyer to exclude the trade amount and fee
     };
 
-    // Add the OP_RETURN payload output if provided
-  if (payload) {
-  // Normalize the payload to ensure no accidental characters
-  const normalizedPayload = payload.trim(); 
-  const data = Buffer.from(normalizedPayload, 'utf8').toString('hex');
-  console.log('Payload Data (hex):', data);
-
-  // Add an OP_RETURN output as part of _outsForRawTx
-  const opReturnOutput = { 
-    script: `6a${data}`, // 6a is the OP_RETURN opcode
-    value: 0,            // OP_RETURN outputs always have a value of 0
-  };
-
-  _outsForRawTx['opReturnOutput'] = opReturnOutput; 
-}
-
-
     console.log('Inputs:', JSON.stringify(_insForRawTx));
     console.log('Outputs:', JSON.stringify(_outsForRawTx));
 
@@ -446,6 +429,7 @@ export const buildLTCTradeTx = async (txConfig: IBuildLTCITTxConfig, isApiMode: 
         };
       }),
       network,
+      payload: payload
     });
 
     if (psbtBuildResult.error) {
