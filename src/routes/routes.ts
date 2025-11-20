01c636bb7b141c17afe4219314f512695e635730
Patrick Dugan <patrick.buchanan.dugan@gmail.com>
2025-11-20 19:38:34 -0300

    Register attestation routes in routes handler

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,13 +4,16 @@ import { chainRoute } from "./chain.route";
 import { rpcRoutes } from "./rpc.route";
 import { tokenRoute } from "./token.route";
 import { txRoute } from "./tx.route";
+import { registerAttestationRoutes } from "./attestation.route";
 
 export const handleRoutes = (server: FastifyInstance) => {
-  
+
     server.register(require('fastify-axios'));
+
     server.register(addressRoute, { prefix: '/address' });
-    server.register(txRoute, { prefix: '/tx' });
-    server.register(tokenRoute, { prefix: '/token' });
-    server.register(chainRoute, { prefix: '/chain' });
-    server.register(rpcRoutes, { prefix: '/rpc' });
-}
+    server.register(txRoute,      { prefix: '/tx' });
+    server.register(tokenRoute,   { prefix: '/token' });
+    server.register(chainRoute,   { prefix: '/chain' });
+    server.register(rpcRoutes,    { prefix: '/rpc' });
+    server.register(registerAttestationRoutes, { prefix: '/attestation' });
+};
